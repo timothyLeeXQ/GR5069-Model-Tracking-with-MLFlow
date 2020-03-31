d5caabb28896ee194da0a033d032a7835517e663
Timothy Lee <timlxq@gmail.com>
2020-03-31 09:36:11 +0800

    add html report, remove redundant imports

--- src/assignment_3.py
+++ src/assignment_3.py
@@ -6,8 +6,6 @@ dbutils.library.installPyPI("mlflow", "1.0.0")
 import boto3
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
 import mlflow.sklearn
 
 # COMMAND ----------
